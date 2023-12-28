3601d201174b03553b4f68dcd18244a9ca6c397c
Arie Schwartzman <ariesch@microsoft.com>
2023-12-28 11:33:54 +0200

    Add API_KEY

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,13 @@ import jwt from 'jsonwebtoken';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const API_KEY = process.env.API_KEY;
+
+// Check if API_KEY is set
+if (!API_KEY) {
+  console.error('API_KEY is not set');
+  process.exit(1);
+}
 
 // Middleware for JSON payload parsing
 app.use(express.json());
@@ -11,6 +18,17 @@ interface IPayload {
   secret: string | undefined;
   audience: string;
 }
+
+// Middleware to check API key
+app.use((req: Request, res: Response, next: Function) => {
+  const apiKey = req.headers['x-api-key'];
+  
+  if (!apiKey || apiKey !== API_KEY) {
+    return res.status(401).json({ message: 'Unauthorized: Invalid API key' });
+  }
+  
+  next();
+});
 
 // Endpoint to create a JWT
 app.post('/sign', (req: Request, res: Response) => {
